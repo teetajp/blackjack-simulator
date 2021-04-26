f695d8226085975b113751db093f0f5a3f7a3cc7
Teetaj Pavaritpong <teetajp@gmail.com>
2021-04-26 17:56:52 -0500

    Implement player.cc

--- src/core/player.cc
+++ src/core/player.cc
@@ -2,4 +2,43 @@
 
 namespace blackjack {
 
+// todo: remove magic numbers
+Player::Player() : name_("Player"), balance_(100), bet_(0) {}
+
+size_t Player::GetBet() const {
+  return bet_;
+}
+
+string Player::GetName() const {
+  return name_;
+}
+
+void Player::Win() {
+  balance_ += 2 * bet_; // won the amount the player bet and return what the player bet
+  bet_ = 0; // reset the bet
+}
+
+void Player::Lose() {
+  bet_ = 0; // Reset the bet without returning the player's bet 
+}
+
+void Player::Push() {
+  balance_ += bet_; // Return the bet, so player doesn't gain or lose anything
+  bet_ = 0; // Reset the bet
+}
+
+void Player::Blackjack() {
+  balance_ += 2.5 * bet_;
+  bet_ = 0;
+}
+
+void Player::PostBet(size_t bet) {
+  bet_ = bet;
+  balance_ -= bet_;
+}
+
+Hand& Player::GetHand() {
+  Hand& hand = hand_;
+  return hand;
+}
 } // namespace blackjack
